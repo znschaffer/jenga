3ba92f023887040e4894f9209d75c58c0bf5aead
Zane Schaffer <personal.zane@gmail.com>
2022-08-03 17:21:10 -0700

    docs: add package comment

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
+/*
+Jenga is a static site builder focused on single page blogs.  It uses a TOML
+file for configuration ( jenga.toml ).  It expects your source files to be
+written in standard Markdown with the extension .md
+
+Usage:
+
+	jenga -config '/path/to/config'
+
+The config flag is optional and if not recieved, jenga will look for the config
+in the current directory
+*/
 package main
 
 import (
